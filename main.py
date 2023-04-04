826bff9602ab17b394c2f3034e472e22edbd295e
kturaevv <kturaevv@gmail.com>
2023-04-04 15:45:57 +0300

    Styling changes

--- main.py
+++ main.py
@@ -5,9 +5,20 @@ from PIL import Image
 def grayscale(image):
     return image.convert('L')
 
-
+# st.set_page_config(layout="wide", page_title="Image Upload", page_icon="🧊")
 st.title("Grayscale Image Converter")
 
+# Disable scrolling and remove menu
+css = """
+        <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            section.main > div:has(~ footer ) {
+                padding-bottom: 5px;
+            }
+        </style>
+        """
+st.markdown(css, unsafe_allow_html=True)
 # Upload an image
 image_file = st.file_uploader("Upload an image", type=['jpg', 'jpeg', 'png'])
 
